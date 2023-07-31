2cb478e414351d8c7abd062b43ba7a69f30c3ed4
TwKnD <43603021+TwKnD@users.noreply.github.com>
2023-07-31 16:26:09 +1000

    Add listener for Enter

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-const searchForm = document.getElementById("search");
 const searchField = document.getElementById("search-field");
 const searchURL = "https://www.google.com/search?q=";
 
 function search() {
     if (searchField.value != "") {
         window.open(searchURL + searchField.value, "_blank")
+        searchField.value = '';
     }
 }
+
+searchField.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        document.getElementById('search-button').click();
+    }
+})
